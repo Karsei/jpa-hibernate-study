a406e41c58cb8fc7a9d1ef208daa61e5e6312ac1
Karsei <karseidev@gmail.com>
2022-10-14 00:08:16 +0900

    feat: added transaction

--- src/main/java/kr/pe/karsei/JpaMain.java
+++ src/main/java/kr/pe/karsei/JpaMain.java
@@ -2,6 +2,7 @@ package kr.pe.karsei;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -12,10 +13,17 @@ public class JpaMain {
         // 고객 요청이 올때마다(DB 커넥션을 얻어서 뭔가 한다던가) 꼭 EntityManager 를 만들어야 한다.
         EntityManager em = emf.createEntityManager();
 
+        // 트랜잭션 - 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
         Member member = new Member();
-        member.setId(1L);
+        member.setId(2L);
         member.setName("mr Hong");
         em.persist(member);
+
+        // 트랜잭션 - 종료
+        tx.commit();
 
         em.close();
         emf.close();
