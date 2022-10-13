a11f7ea53d760b5672d7745bccada75235124779
Karsei <karseidev@gmail.com>
2022-10-14 00:08:05 +0900

    refactor: changed getter and setter with lombok

--- src/main/java/kr/pe/karsei/Member.java
+++ src/main/java/kr/pe/karsei/Member.java
@@ -1,27 +1,15 @@
 package kr.pe.karsei;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
 public class Member {
     @Id // Primary Key
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
