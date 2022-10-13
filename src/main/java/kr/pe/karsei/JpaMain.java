fc2a363ff601afdcf4a0b4dccdf2e87605b5ef1b
Karsei <karseidev@gmail.com>
2022-10-14 00:17:40 +0900

    feat: added crud and fixed some docs

--- src/main/java/kr/pe/karsei/JpaMain.java
+++ src/main/java/kr/pe/karsei/JpaMain.java
@@ -8,9 +8,10 @@ import javax.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         // persistence.xml 에 있는 이름
-        // 어플리케이션에서 딱 하나만 생성되어야 한다.
+        // 어플리케이션에서 딱 하나만 생성되어야 하고, 전체에서 공유되어야 한다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gazuua");
         // 고객 요청이 올때마다(DB 커넥션을 얻어서 뭔가 한다던가) 꼭 EntityManager 를 만들어야 한다.
+        // 스레드간에 절대 공유가 되면 안 된다. (사용 후 버려야 함)
         EntityManager em = emf.createEntityManager();
 
         // 트랜잭션 - 시작
@@ -18,10 +19,23 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+            // 수정
+            // 모든 데이터 변경은 트랜잭션 안에서 실행되어야 한다.
+            findMember.setName("Mr Hong1");
+
+            // 추가
             Member member = new Member();
-            member.setId(2L);
-            member.setName("mr Hong");
+            member.setId(3L);
+            member.setName("Mr Hong3");
             em.persist(member);
+
+            // 삭제
+            em.remove(member);
 
             // 트랜잭션 - 종료
             tx.commit();
